579548a1456f91075bcadea94289e213ecadbd75
sak-sh-i <codeblooded343@gmail.com>
2021-07-27 12:38:53 +0530

    Add dotenv

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var mongoose = require('mongoose');
 var path = require('path');
+const dotenv = require("dotenv");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
@@ -9,6 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
 var app = express();
+
+dotenv.config({ path: "./config.env" });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
